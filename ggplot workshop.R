37a802f94636d8c6fa133348a817063a718a4c5f
Natasha chik <natashachikhy@gmail.com>
2024-05-15 00:46:07 +1000

    update

---
+++ ggplot workshop.R
@@ -0,0 +1,17 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+mpg
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+install.packages(c("pkg1", "pkg2"))
+install.packages("Rcmdr", dependencies = TRUE)
+
+ggplot() 
+install.packages('ggplot')
+library(ggplot)
