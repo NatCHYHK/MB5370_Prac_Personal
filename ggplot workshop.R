d5215febd4911b9bd0061ee6b32039b80b9e86de
Natasha chik <natashachikhy@gmail.com>
2024-05-16 09:33:43 +1000

    update

--- ggplot workshop.R
+++ ggplot workshop.R
@@ -15,3 +15,9 @@ install.packages("Rcmdr", dependencies = TRUE)
 ggplot() 
 install.packages('ggplot2')
 library(ggplot2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class, alpha = 1/10))
+
+ggplot(data = mpg) + 
+  geom_point(x = displ, y = hwy, alpha = class)
